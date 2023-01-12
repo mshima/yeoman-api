8dfafed0788e714dc6a08c56c902468bce4959a8
Marcelo Shima <marceloshima@gmail.com>
2023-01-12 17:18:22 -0300

    fix: change the default types to types.d.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-export type { default as BaseEnvironment } from './environment.js';
-export type { default as BaseGenerator } from './generator.js';
-export type { default as BaseGenerator6 } from './generator6.js';
-export * from './generator-definitions.js';
-export * from './generator-factory.js';
-export * from './generator-features.js';
-export * from './generator-options.js';
-
-export * from './adapter.js';
-export * from './logger.js';
 export * from './namespace.js';
---
+++ src/types.d.ts
@@ -0,0 +1,13 @@
+export * from './index.d.js';
+
+export * from './adapter.js';
+export * from './logger.js';
+
+export type { default as BaseEnvironment } from './environment.js';
+export type { default as BaseGenerator } from './generator.js';
+export type { default as BaseGenerator6 } from './generator6.js';
+
+export * from './generator-definitions.js';
+export * from './generator-factory.js';
+export * from './generator-features.js';
+export * from './generator-options.js';
